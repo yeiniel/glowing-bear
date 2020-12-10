335b844418b36c23cdb6ffd5c79c7ef4e38b39f1
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2020-12-10 11:47:13 -0500

    convert latexmath filter to typescript
    additionally fix a missing dependency issue

--- src/js/latexmath.filter.ts
+++ src/js/latexmath.filter.ts
@@ -1,15 +1,16 @@
 
-'use strict';
+import katex from 'katex';
+import renderMathInElement from 'katex/dist/contrib/auto-render';
 
 export function latexmathFilter() {
-    return function(text, selector, enabled) {
+    return function(text: string, selector: string, enabled: boolean) {
         if (!enabled || typeof(katex) === "undefined") {
             return text;
         }
         if (text.indexOf("$$") != -1 || text.indexOf("\\[") != -1 || text.indexOf("\\(") != -1) {
             // contains math -> delayed rendering
             setTimeout(function() {
-                var math = document.querySelector(selector);
+                var math = document.querySelector(selector) as HTMLElement;
                 renderMathInElement(math, {
                     delimiters: [
                         {left: "$$", right: "$$", display: false},
